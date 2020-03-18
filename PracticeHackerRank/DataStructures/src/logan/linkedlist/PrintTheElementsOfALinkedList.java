480c8f8b394b98a46f2ce3ae82ff7972be745bc1
Long Phan <loganphanxnt@yahoo.com>
2020-03-18 16:44:52 +0700

    Update PrintTheElementsOfALinkedList.java

--- PracticeHackerRank/DataStructures/src/logan/linkedlist/PrintTheElementsOfALinkedList.java
+++ PracticeHackerRank/DataStructures/src/logan/linkedlist/PrintTheElementsOfALinkedList.java
@@ -12,15 +12,24 @@ public class PrintTheElementsOfALinkedList {
 	/**
 	 * @param args
 	 */
+	 /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 	
-	void Print(Node head) {
-		  
-		  
-	}
-	
-	
+   	static void printLinkedList(SinglyLinkedListNode head) {
+	    System.out.println(head.data);
+	    if(head.next != null){
+                printLinkedList(head.next);
+      	    }
+    }
 }
